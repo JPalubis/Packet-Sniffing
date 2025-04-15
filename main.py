b897a6f59d8c1a21a163187f8163043db8943690
jlpal <jlpalubiski@gmail.com>
2025-04-15 17:50:28 -0400

    added main function

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import socket
 import struct
 import textwrap
 
+def main():
+    connection = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, address = connection.recvfrom(65535) # This is the biggest buffer size
+        destination_mac, source_mac, ethernet_protocol, data = ethernet_frame(raw_data)
+        print("\nEthernet Frame:")
+        print(f"Destination: {destination_mac}, Source: {source_mac}, Protocol: {ethernet_protocol}")
+
 # Unpack ethernet frame
 def ethernet_frame(data):
     # The sender and receiver in the ethernet frame are 6 bytes, and the TYPE is 2 bytes
@@ -12,3 +21,6 @@ def ethernet_frame(data):
 def get_mac_address(bytes_address):
     bytes_str = map('{:02x}'.format, bytes_address)
     return ':'.join(bytes_str).upper()
+
+
+main()
