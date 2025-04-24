e1c02e2101b313f9d8fef0f41c8b25ba64dbe19b
jlpal <jlpalubiski@gmail.com>
2025-04-24 14:18:52 -0400

    finished tests for helper functions

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from main import (ethernet_frame, get_mac_address, ipv4_packet, ipv4, icmp_packe
 
 class TestPacketSniffer(unittest.TestCase):
 
+    # Helper function tests
     def test_get_mac_address(self):
         mac_bytes = b'\xaa\xbb\xcc\xdd\xee\xff'
         self.assertEqual(get_mac_address(mac_bytes), 'AA:BB:CC:DD:EE:FF')
@@ -14,7 +15,12 @@ class TestPacketSniffer(unittest.TestCase):
         ip_bytes = b'\xc0\xa8\x01\x01'
         self.assertEqual(ipv4(ip_bytes), '192.168.1.1')
     
-    def test_format_mult_line(self):
+    def test_format_mult_line_bytes(self):
         data = b'\x01\x02\x03\x04'
         expected = r'\x01\x02\x03\x04'
         self.assertEqual(format_mult_line('', data), expected)
+    
+    def test_format_mult_line_string(self):
+        data = 'A' * 100
+        result = format_mult_line('', data, size = 20)
+        self.assertTrue(all(len(line) <= 20 for line in result.split('\n')))
